6943f219f5c0d7ba9fd96bf267a3a81fe1019190
ALAM <alam.2008@live.co.uk>
2024-05-06 01:15:39 +0100

    Authentication - Fixing a important bugs

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,7 +9,8 @@ export function useLogin() {
     const { mutate: login, isLoading } = useMutation({
         mutationFn: ({ email, password }) => loginApi({ email, password }),
         onSuccess: (user) => {
-            queruClinent.setQueriesData(["user"], user);
+            console.log(user);
+            queruClinent.setQueryData(["user"], user.user);
             navigate("/dashboard", { replace: true });
         },
 
