796f5c57139a0bf472344d914092b4438d10ab6b
ALAM <alam.2008@live.co.uk>
2024-05-06 15:51:36 +0100

    Building the Dashboard Layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `;
+
+export function DashboardLayout() {
+    return (
+        <StyledDashboardLayout>
+            <div>Dashboard</div>
+            <div>activities</div>
+            <div>stay duration</div>
+            <div>Charts</div>
+        </StyledDashboardLayout>
+    );
+}
