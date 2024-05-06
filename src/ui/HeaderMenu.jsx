e4f1e98125a34cac6214fa5c9dcab14cdb58e588
ALAM <alam.2008@live.co.uk>
2024-05-06 09:53:37 +0100

    Building the App Header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon.jsx";
+import { HiOutlineUser } from "react-icons/hi";
+import Logout from "../features/authentication/Logout.jsx";
+import { useNavigate } from "react-router-dom";
+
+const StyledHeaderMenu = styled.ul`
+    display: flex;
+    gap: 0.4rem;
+`;
+
+export function HeaderMenu() {
+    const navigate = useNavigate();
+    return (
+        <StyledHeaderMenu>
+            <li>
+                <ButtonIcon onClick={() => navigate("/account")}>
+                    <HiOutlineUser />
+                </ButtonIcon>
+            </li>
+            <li>
+                <Logout />
+            </li>
+        </StyledHeaderMenu>
+    );
+}
+
+export default HeaderMenu;
